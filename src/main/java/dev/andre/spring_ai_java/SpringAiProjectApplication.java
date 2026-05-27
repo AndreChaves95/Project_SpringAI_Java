5b10f29e96ddcaf5abf710dcab194a35d1509ae0
andre.chaves <andrefmoreirachaves@hotmail.com>
2026-05-27 12:36:55 +0100

    T1: Initialize project

---
+++ src/main/java/dev/andre/spring_ai_java/SpringAiProjectApplication.java
@@ -0,0 +1,13 @@
+package dev.andre.spring_ai_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/andre/spring_ai_java/SpringAiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.andre.spring_ai_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
