112be77e11da0e956e5cb4a77c604ba6c2897743
andre.chaves <andrefmoreirachaves@hotmail.com>
2026-08-06 12:34:06 +0100

    T6: Add Image generation

--- src/main/java/dev/andre/spring_ai_java/controller/AiModelController.java
+++ src/main/java/dev/andre/spring_ai_java/controller/AiModelController.java
@@ -3,6 +3,8 @@ package dev.andre.spring_ai_java.controller;
 import dev.andre.spring_ai_java.service.ChatModelService;
 import dev.andre.spring_ai_java.service.ImageModelService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController             // Class handles the request and returns data directly
@@ -19,8 +21,15 @@ public class AiModelController {
         return chatModelService.generateText(prompt);
     }
 
-    @PostMapping("/generateImage")
-    public String generateAiImage(@RequestParam String prompt) {
-        return imageModelService.generateImage(prompt);
+    // Request for OpenAI which requires a paid subscription
+    @PostMapping(value = "/generate-image", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<byte[]> generateOpenAiImage(@RequestParam String prompt) {
+        return ResponseEntity.ok(imageModelService.generateOpenAiImage(prompt));
+    }
+
+    // Request for StabilityAI which has a free subscription
+    @PostMapping(value = "/generateAiImage", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<byte[]> generateStabilityAiImage(@RequestParam String prompt) {
+        return ResponseEntity.ok(imageModelService.generateStabilityAiImage(prompt));
     }
 }
--- src/main/java/dev/andre/spring_ai_java/service/ImageModelService.java
+++ src/main/java/dev/andre/spring_ai_java/service/ImageModelService.java
@@ -1,25 +1,60 @@
 package dev.andre.spring_ai_java.service;
 
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.image.ImageModel;
 import org.springframework.ai.image.ImagePrompt;
 import org.springframework.ai.image.ImageResponse;
 import org.springframework.ai.openai.OpenAiImageOptions;
+import org.springframework.ai.stabilityai.api.StabilityAiImageOptions;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.util.Base64;
 import java.util.Objects;
 
 @Service
-@RequiredArgsConstructor
+@Slf4j
 public class ImageModelService {
 
-    private final ImageModel imageModel;
+    private final ImageModel opeanAiImageModel;
+    private final ImageModel stabilityAiImageModel;
 
-    public String generateImage(String prompt) {
-        ImageResponse imageResponse = imageModel.call(new ImagePrompt(prompt,
+    // Using Qualifier so Spring knows which model bean should be used
+    public ImageModelService(@Qualifier("openAiImageModel") ImageModel opeanAiImageModel,
+                             @Qualifier("stabilityAiImageModel") ImageModel stabilityAiImageModel) {
+        this.opeanAiImageModel = opeanAiImageModel;
+        this.stabilityAiImageModel = stabilityAiImageModel;
+    }
+
+    //Here is service for OpenAI which requires a paid subscription
+    public byte[] generateOpenAiImage(String prompt) {
+
+        log.info("Generating OpenAI image for prompt: {}", prompt);
+
+        ImageResponse imageResponse = opeanAiImageModel.call(new ImagePrompt(prompt,
                 OpenAiImageOptions.builder()
+                        .quality("hd")
+                        .responseFormat("b64_json")
                         .build()));
 
-        return Objects.requireNonNull(imageResponse.getResult()).getOutput().getUrl();
+        String b64Json = Objects.requireNonNull(imageResponse.getResult()).getOutput().getB64Json();
+        return Base64.getDecoder().decode(b64Json);
+    }
+
+    //Here is service for Stability AI which is free to use
+    public byte[] generateStabilityAiImage(String prompt) {
+
+        log.info("Generating Stability AI image for prompt: {}", prompt);
+
+        ImageResponse imageResponse = stabilityAiImageModel.call(new ImagePrompt(prompt,
+                StabilityAiImageOptions.builder()
+                        .stylePreset("cinematic")
+                        .N(1)
+                        .height(512)
+                        .width(512)
+                        .build()));
+
+        String b64Json = Objects.requireNonNull(imageResponse.getResult()).getOutput().getB64Json();
+        return Base64.getDecoder().decode(b64Json);
     }
 }
